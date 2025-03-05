dccfd17c04e24eec9c7e1c835fb41787a147f338
Maria Castro <mcastrod08@gmail.com>
2025-03-05 13:46:45 +0100

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="bg-black text-blue-500 text-3xl">App</div>
+    <div className="bg-black text-blue-500 text-3xl">
+      <h1>App1</h1>
+    
+    </div>
   )
 }
 
