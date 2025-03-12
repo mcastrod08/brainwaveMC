f62b50d480683830dd138ac3ebd4095250da8126
Maria Castro <mcastrod08@gmail.com>
2025-03-12 13:12:20 +0100

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Section from './Section'
 import { curve, heroBackground, robot } from '../assets'
 import Button from './Button'
-import { BackgroundCircles } from './design/HamburgerMenu'
+import { BackgroundCirclesHero, BottomLine, Gradient } from './design/Hero'
+import { heroIcons } from '../constants'
+import { ScrollParallax } from 'react-just-parallax'
+import Generating from "./Generating";
+import Notification from './Notification'
 
 const Hero = () => {
+  const parallaxRef = useRef(null);
+
   return (
     <Section
       className="pt-[12rem] -mt-[5.25rem]"
@@ -13,8 +19,8 @@ const Hero = () => {
       customPaddings
       id="hero"
     >
-      <BackgroundCircles/>
-      <div className="container relative">
+       <BackgroundCirclesHero/>
+      <div className="container relative" ref={parallaxRef}>
         <div className="relative z-10 max-w-[62rem] mx-auto text-center mb-[4rem]
           md:mb-20 lg:mb:[6rem] mt-20">
           <h1>Explore the Possibilities of 
@@ -28,40 +34,54 @@ const Hero = () => {
               />
               
             </span>
-          </h1>
+          </h1> 
           <p className="mt-20 max-w-2xl mx-auto mb-6 text-gray-400">Unleash the power of AI within Brainwave. Upwrade your productivity with Brainwave, the open AI chat app.</p>
           <Button white href="/pricing">
             Get started
           </Button>  
         </div>
-        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+        
+        
+        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24"> 
           <div className="relative z-0 p-0.5 rounded-2xl bg-conic from-gray-600 to-[#AC6AFF] to-50%">
-            <div className="relative bg-[#0E0C15] rounded-[1rem] z-0 ">
-              <div className="h-[1.4rem] bg-[#43435C] rounded-t-[0.9rem] "/>
-              <div className='aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]'>
+            <div className="relative bg-[#0E0C15] rounded-[1rem] z-0 "> 
+              <div className="h-[1.4rem] bg-[#43435C] rounded-t-[0.9rem] "/> 
+              <div className='aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]'><BackgroundCirclesHero/>
                 <img 
                   src={robot}
-                  className="relative z-30 w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
+                  className="relative  w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
                   height={490}
                   width={1024}
                   alt="hero"
                 />
+                <ScrollParallax isAbsolutelyPositioned>
+                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
+                </ScrollParallax>
+                
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-gray-600/50 border backdrop-blur border-[#AC6AFF]  rounded-2xl xl:flex">
+                    {heroIcons.map((icon, index) => (
+                      <li className="p-5" key={index}>
+                        <img src={icon} width={24} height={25} alt={icon}/>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollParallax>
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <Notification className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex" title="Code generation"/>
+                </ScrollParallax>
               </div>
-              <div className="absolute z-0 -top-[54%]  left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
-                <img
-                  src={heroBackground}
-                  className=" w-full"
-                  width={500}
-                  height={100}
-                  alt="hero"
-                /> 
-              </div>
+              
             
                
             
             </div>
+            <Gradient/>
           </div>
+          
         </div>
+        
       </div>
     </Section>
   )
